5977a3bfc5710edfdd0afc9a1724d2da0f166970
Zaman Ehsani <zamanehsani@gmail.com>
2022-08-04 02:59:40 +0400

    modified for deployment

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+var app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(express.static(__dirname + '/'));
+app.listen(process.env.PORT || 8080);
+
+//production mode
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join((__dirname + "/build/index.html")));
+  });
+}
+
+//build mode
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "public/index.html"));
+});    
