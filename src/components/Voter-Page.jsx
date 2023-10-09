5e2082e00f28414a2793e0fc1eeef8d79342fec0
kylebrackman <kyle.a.brackman@gmail.com>
2023-10-09 17:17:52 -0600

    first changes to voter page design

--- src/components/Voter-Page.jsx
+++ src/components/Voter-Page.jsx
@@ -1,75 +1,78 @@
-import {Link} from 'react-router-dom';
-import {useSelector,useDispatch} from 'react-redux';
-import {useNavigate, useHistory} from 'react-router-dom';
-import {useState, useEffect} from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate, useHistory } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
-import {pod,authenticate} from '../store/userSlice.js';
-import {baseURL} from '../store/conf.js'
+import { pod, authenticate } from '../store/userSlice.js';
+import { baseURL } from '../store/conf.js'
 import jwtDecode from 'jwt-decode';
+import Button from 'react-bootstrap/Button';
+import { Container, Row, Col } from 'react-bootstrap';
 
-function VoterPage(){
+
+function VoterPage() {
     const AuthUser = useSelector((state) => state.AuthUser.user);
-    const [message, setMessage] = useState({type:'alert alert-',msg:''});
+    const [message, setMessage] = useState({ type: 'alert alert-', msg: '' });
     const [token, setToken] = useState('');
-    const [action,setAction] = useState('');
+    const [action, setAction] = useState('');
     const [pageLoaded, setPageLoaded] = useState(false)
     const dispatch = useDispatch();
     const navigate = useNavigate();
-    const podMembers    = useSelector((state) => state.AuthUser.podMembers);
-    const [delegate, setDelegate] = useState(podMembers?.filter((e)=> e.is_delegate === true)[0]);
-    
+    const podMembers = useSelector((state) => state.AuthUser.podMembers);
+    const [delegate, setDelegate] = useState(podMembers?.filter((e) => e.is_delegate === true)[0]);
+
     let date = new Date(AuthUser.date_joined)
 
 
     // get the new access token on each page load or redirect
-    useEffect(()=>{
+    useEffect(() => {
         const TokenUrl = `${window.location.protocol}//${baseURL}/api/token/refresh/`;
-        const token_params = {refresh: AuthUser.token.refresh}
+        const token_params = { refresh: AuthUser.token.refresh }
         axios.post(TokenUrl, token_params)
-        .then(response =>{
-            if(response.status === 200){
-                // set the new access token and which btn is clicked via clicked const.
-                setToken(response.data.access);
-                let u = {...AuthUser}
-                u.token = {refresh: AuthUser.token.refresh, access: response.data.access}
-                dispatch(authenticate(u))
-                console.log("token is set on token and authuser.token")
-               
-                setAction('userinfo')
-            }else{
-                setMessage({type:"alert alert-danger",msg:"could not get access token"})
-            }
-        })
-        .catch(error => {
-            setMessage({type:"alert alert-danger",msg:"something went wrong with your request"})
-            // setErr("Something went wrong. Check your inputs and try again.");
-            console.log(error)
-        }); 
-    },[])
+            .then(response => {
+                if (response.status === 200) {
+                    // set the new access token and which btn is clicked via clicked const.
+                    setToken(response.data.access);
+                    let u = { ...AuthUser }
+                    u.token = { refresh: AuthUser.token.refresh, access: response.data.access }
+                    dispatch(authenticate(u))
+                    console.log("token is set on token and authuser.token")
 
-    useEffect(()=>{
-        switch(action){
+                    setAction('userinfo')
+                } else {
+                    setMessage({ type: "alert alert-danger", msg: "could not get access token" })
+                }
+            })
+            .catch(error => {
+                setMessage({ type: "alert alert-danger", msg: "something went wrong with your request" })
+                // setErr("Something went wrong. Check your inputs and try again.");
+                console.log(error)
+            });
+    }, [])
+
+    useEffect(() => {
+        switch (action) {
             // if no action is specified, fetch user info and pod
             case 'userinfo':
-                if(AuthUser.token.access.length > 0){
+                if (AuthUser.token.access.length > 0) {
                     const url = `${window.location.protocol}//${baseURL}/api/userinfo/`;
-                    const params = {user: AuthUser.username}
-                    let header = {'Authorization': `Bearer ${AuthUser.token.access}`}
-                    axios.post(url, params, {headers: header})
-                    .then(response => {
-                        if(response.status === 200){
-                            let token = AuthUser.token
-                            dispatch(authenticate({...response.data.user,token}))
-                            if(response.data.user.users.userType===1){
-                                dispatch(pod(response.data.pod))
+                    const params = { user: AuthUser.username }
+                    let header = { 'Authorization': `Bearer ${AuthUser.token.access}` }
+                    axios.post(url, params, { headers: header })
+                        .then(response => {
+                            if (response.status === 200) {
+                                let token = AuthUser.token
+                                dispatch(authenticate({ ...response.data.user, token }))
+                                if (response.data.user.users.userType === 1) {
+                                    dispatch(pod(response.data.pod))
+                                }
+                            } else {
+                                setMessage({ type: "alert alert-danger", msg: "could not get user info and pod" })
                             }
-                        }else{
-                            setMessage({type:"alert alert-danger",msg:"could not get user info and pod"})
-                        }
-                    })
-                    .catch(error => {
-                        console.log("error on fetching user and pod info:",error)
-                    });
+                        })
+                        .catch(error => {
+                            console.log("error on fetching user and pod info:", error)
+                        });
                 }
                 break
             case 'joinPd':
@@ -77,58 +80,58 @@ function VoterPage(){
                 break
             case 'createPod':
                 console.log("creating a pod")
-                if(token.length > 0){
-                    let header = {'Authorization': `Bearer ${token}`}
+                if (token.length > 0) {
+                    let header = { 'Authorization': `Bearer ${token}` }
                     const url = `${window.location.protocol}//${baseURL}/api/create-pod/`
-                    const param = {"user": AuthUser.username, 'district':AuthUser.users.district.code}
-                    axios.post(url, param, {headers:header})
-                    .then( response => {
-                        if(response.status === 400 ){
-                            setMessage({msg:response.data.message,type: "alert alert-danger" })
-                        }else if(response.status === 200){
-                            dispatch(pod(response.data))
-                            let u = {...AuthUser}
-                            u.userType = 1
-                            dispatch(authenticate(u))
-                            setMessage({type:"alert alert-success", msg:"pod created."})
-                            // nagivate to voter page...
-                            navigate('/house-keeping-page');
-                        }else{
-                            console.log("something went wrong:", response)
-                        }
-                    })
-                    .catch(error => {
-                        console.log("err: ", error)
-                        setMessage({msg:error.response?.data?.message, type:"alert alert-danger"})
-                    });
+                    const param = { "user": AuthUser.username, 'district': AuthUser.users.district.code }
+                    axios.post(url, param, { headers: header })
+                        .then(response => {
+                            if (response.status === 400) {
+                                setMessage({ msg: response.data.message, type: "alert alert-danger" })
+                            } else if (response.status === 200) {
+                                dispatch(pod(response.data))
+                                let u = { ...AuthUser }
+                                u.userType = 1
+                                dispatch(authenticate(u))
+                                setMessage({ type: "alert alert-success", msg: "pod created." })
+                                // nagivate to voter page...
+                                navigate('/house-keeping-page');
+                            } else {
+                                console.log("something went wrong:", response)
+                            }
+                        })
+                        .catch(error => {
+                            console.log("err: ", error)
+                            setMessage({ msg: error.response?.data?.message, type: "alert alert-danger" })
+                        });
                 }
                 break
             default:
                 console.log("defualt is here")
         }
-    },[action])
+    }, [action])
 
     useEffect(() => {
-      const isTokenExpired = () => {
-        const decodedToken = jwtDecode(AuthUser.token.refresh);
-        return decodedToken.exp < Date.now() / 1000;
-      };
-  
-      if (isTokenExpired()) {
-        navigate('/enter-the-floor');
-      }
+        const isTokenExpired = () => {
+            const decodedToken = jwtDecode(AuthUser.token.refresh);
+            return decodedToken.exp < Date.now() / 1000;
+        };
+
+        if (isTokenExpired()) {
+            navigate('/enter-the-floor');
+        }
     }, []);
 
     const houseKeepingType = () => {
-        switch(AuthUser?.users?.userType){
-            case 0 :
+        switch (AuthUser?.users?.userType) {
+            case 0:
                 return (
                     <div className="row text-center">
                         <div className="col-sm-12 ">
                             <Link to={'/join-pod'} className="btn btn-primary m-2">Join a Pod</Link>
                         </div>
                         <div className="col-sm-12 ">
-                            <a onClick={()=>setAction('createPod')} className="btn btn-primary m-2">Create a Pod</a>
+                            <a onClick={() => setAction('createPod')} className="btn btn-primary m-2">Create a Pod</a>
                         </div>
                     </div>
                 )
@@ -139,22 +142,22 @@ function VoterPage(){
                             <Link to='/house-keeping-page' className="btn btn-primary m-2"> My pod</Link>
                         </div>
                         {/* add if the user is delegate and then show this two. */}
-                        { AuthUser?.username === delegate?.user?.username ? <>
-                        <div className="col-sm-12 col-md-6 col-lg-6">
-                            <a className="btn btn-primary m-2" >Join First Link</a>
-                        </div>
-                        <div className="col-sm-12 col-md-6 col-lg-6">
-                            <a className="btn btn-primary m-2" >Create First Link</a>
-                        </div>
+                        {AuthUser?.username === delegate?.user?.username ? <>
+                            <div className="col-sm-12 col-md-6 col-lg-6">
+                                <a className="btn btn-primary m-2" >Join First Link</a>
+                            </div>
+                            <div className="col-sm-12 col-md-6 col-lg-6">
+                                <a className="btn btn-primary m-2" >Create First Link</a>
+                            </div>
                         </>
-                        : ""}
+                            : ""}
                         <div className="col-sm-12 col-md-6 col-lg-6">
-                        <Link  className='btn btn-primary m-2' to={'/pod-back-n-forth'}>Back-and-Forth</Link>
+                            <Link className='btn btn-primary m-2' to={'/pod-back-n-forth'}>Back-and-Forth</Link>
                             {/* <a className="btn btn-primary m-2" >Back-and-Forth</a> */}
                         </div>
                     </div>
                 )
-            
+
             default:
                 return (
                     <div className='row text-center'>
@@ -166,46 +169,62 @@ function VoterPage(){
 
     return (
         <div className="container">
-             <div className="row">
+            <div className="row">
                 <div className="col">
                     {message.msg ?
-                        <div  className={message?.type} role="alert"> {message?.msg}</div>
-                    :""}
+                        <div className={message?.type} role="alert"> {message?.msg}</div>
+                        : ""}
 
-                    {AuthUser?.users?.is_reg ? 
-                    <div className="alert alert-warning mt-3">
-                        <p>
-                            You have to got until <span className='text-danger'>{date.toDateString()}</span> to
-                            registered to vote in your district or your account will be deleted! Yikes! 
-                            <a href="#"> Learn More</a>. 
-                        </p>
-                        <p>
-                            But, if you have gotten registered since the last time you entered the floor,
-                            <a href="#"> click here</a>.  
-                        </p>
-                    </div>
-                    :""}
+                    {AuthUser?.users?.is_reg ?
+                        <div className="alert alert-warning mt-3">
+                            <p>
+                                You have to got until <span className='text-danger'>{date.toDateString()}</span> to
+                                registered to vote in your district or your account will be deleted! Yikes!
+                                <a href="#"> Learn More</a>.
+                            </p>
+                            <p>
+                                But, if you have gotten registered since the last time you entered the floor,
+                                <a href="#"> click here</a>.
+                            </p>
+                        </div>
+                        : ""}
                 </div>
             </div>
 
-            <div className="row mt-3">
-                <div className="col-sm-12 col-md-12 col-lg-12">
-                    <h1 className="text-center">Voter Page for {AuthUser?.users?.legalName}</h1>
+            {/* <div className="row mt-3">
+                <div className="col-sm-12 col-md-12 col-lg-12 row">
+                    <div className="col-sm-6">
+                        <p>Voter:</p>
+                    </div>
+                    <div className="col-sm-6">
+                        <h1 >{AuthUser?.users?.legalName}</h1>
+                    </div>
                     <h3 className="text-center">Your Verification Score: {AuthUser?.users?.verificationScore}/7</h3>
                 </div>
                 <div className="col-sm-12 col-md-12 col-lg-12">
                     <h1 className="text-center"> District: {AuthUser.users?.district?.code} </h1>
                 </div>
-            </div>
+            </div> */}
+
+            <Container>
+                <Row>
+                    <Col xs="auto">
+                        <p className="text-left">Voter:</p>
+                    </Col>
+                    <Col>
+                        <h1>John Doe</h1>
+                    </Col>
+                </Row>
+            </Container>
 
             {/* the three columns on  */}
             <div className="row">
                 <div className="col-sm-12 col-md-4 d-flex justify-content-center">
                     <ul className="list-unstyled">
-                        <li><Link  to={'/voter-page'}>List of Delegates</Link></li>
-                        <li><Link  to={'/pod-back-n-forth'}>Back-and-Forth</Link></li>
-                        <li><Link  to={'/voter-page'}> Member Contact Page </Link></li>
-                        <li><Link  to={'/house-keeping-page'}>Pod Housekeeping Page</Link></li>
+                        <li className="mb-2"><Link to={'/voter-page'}><Button variant="dark"> List of Delegates</Button></Link></li>
+                        <li className="mb-2"><Link to={'/pod-back-n-forth'}><Button variant="dark"> Back-and-Forth </Button></Link></li>
+                        <li className="mb-2"><Link to={'/voter-page'}> <Button variant="dark"> Member Contact Page </Button> </Link></li>
+                        <li className="mb-2"><Link to={'/house-keeping-page'}> <Button variant="dark"> Pod Housekeeping Page </Button></Link></li>
                     </ul>
                 </div>
                 <div className="col-sm-12 col-md-4 d-flex justify-content-center">
@@ -213,14 +232,14 @@ function VoterPage(){
                 </div>
                 <div className="col-sm-12 col-md-4 d-flex justify-content-center">
                     <ul className="list-unstyled">
-                        <li><Link  to={'/voter-page'}>First Link Meeting Schedule</Link></li>
-                        <li><Link  to={'/voter-page'}>Meeting Minutes Log</Link></li>
-                        <li><Link  to={'/voter-page'}>Bill Metrics </Link></li>
-                        <li><Link  to={'/voter-page'}>Voter Settings</Link></li>
+                        <li className="mb-2"><Link to={'/voter-page'}> <Button variant="dark"> First Link Meeting Schedule </Button></Link></li>
+                        <li className="mb-2"><Link to={'/voter-page'}> <Button variant="dark"> Meeting Minutes Log </Button></Link></li>
+                        <li className="mb-2"><Link to={'/voter-page'}> <Button variant="dark"> Bill Metrics </Button> </Link></li>
+                        <li className="mb-2"><Link to={'/voter-page'}> <Button variant="dark"> Voter Settings </Button></Link></li>
                     </ul>
                 </div>
             </div>
-           
+
         </div>
     )
 }
