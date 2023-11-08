40a4aa41e496c1288cc8ab255419589320334b5b
kylebrackman <kyle.a.brackman@gmail.com>
2023-11-08 10:43:30 -0700

    drafting settings page to be ready to be connected to backend

--- src/components/SettingsPage.jsx
+++ src/components/SettingsPage.jsx
@@ -1,9 +1,25 @@
+import { Container, Form, Table } from "react-bootstrap";
 
+function SettingsPage() {
 
-function SettingsPage () {
     return (
         <div>
-            <h1>Settings</h1>
+            <Container>
+                <h2>User Settings</h2>
+                <div>
+                    <Table striped bordered hover responsive>
+                        <tbody>
+                            <td>Bold Font</td>
+                            <td>Bold the font of entire application to increase visibility.</td>
+                            <td>
+                                <Form>
+                                    <Form.Check type="switch" id="custom-switch" />
+                                </Form>
+                            </td>
+                        </tbody>
+                    </Table>
+                </div>
+            </Container>
         </div>
     )
 }
