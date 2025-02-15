5dd2d020fe49f931e3b2452949ff97f830b133c6
Zaman Ehsani <zamanehsani@gmail.com>
2025-02-15 19:57:02 +0400

    fixed the contact page and made it working

--- src/components/MemberContactPage.jsx
+++ src/components/MemberContactPage.jsx
@@ -1,21 +1,51 @@
-import React from "react";
-import {useSelector, } from 'react-redux';
+import { useSelector } from "react-redux";
+import ContactInfoItem from "./contact_info_item.jsx";
+import { useEffect, useState } from "react";
+import { baseURL } from "../store/conf.js";
+import axios from "axios";
+
 function MemberContactPage() {
-  // const AuthUser      = useSelector((state) => state.AuthUser.user);
-  const circleInfo       = useSelector((state) => state.AuthUser.circle);
-  const circleMembers    = useSelector((state) => state.AuthUser.circleMembers);
+  const AuthUser = useSelector((state) => state.AuthUser.user);
+  const circleInfo = useSelector((state) => state.AuthUser.circle);
+  const circleMembers = useSelector((state) => state.AuthUser.circleMembers);
+  const [contactList, setContactList] = useState([]);
+
+  const isDelegate = () => {
+    return circleMembers.some(
+      (member) =>
+        member.user.username === AuthUser.username && member.is_delegate
+    );
+  };
+
+  const fetchContactList = async () => {
+    try {
+      let header = { Authorization: `Bearer ${AuthUser.token.access}` };
+      let url = `${window.location.protocol}//${baseURL}/api/contact-info/`;
+      const response = await axios.get(url, { headers: header });
+      // console.log("Contact List: ", response.data);
+      setContactList(response.data);
+    } catch (error) {
+      console.error("Error fetching contact list: ", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchContactList();
+  }, []);
 
   return (
     <div className="container">
       <div className="row">
         <div className="col-sm-12 col-md-3"></div>
-          <div className="col-sm-12 col-md-6 mt-3">
-              {/* <div className="row">
+        <div className="col-sm-12 col-md-6 mt-3">
+          {/* <div className="row">
                   <div class="alert alert-danger" role="alert"> </div>
               </div> */}
-              <h1 className="text-center">Members Contant Page </h1>
-              <h3 className='text-center'>Circle: {circleInfo?.code} District: {circleInfo?.district?.code}</h3>
-          </div>
+          <h1 className="text-center">Members Contant Page </h1>
+          <h3 className="text-center">
+            Circle: {circleInfo?.code} District: {circleInfo?.district?.code}
+          </h3>
+        </div>
         <div className="col-sm-12 col-md-3"></div>
       </div>
 
@@ -23,41 +53,28 @@ function MemberContactPage() {
         {/* make a table with columns of No, Legal Name, Address, Contact Info, Contact Rules */}
         <table className="table table-bordered">
           <thead>
-              <tr className="">
-                <th className="fs-5">No</th>
-                <th className="fs-5">Legal Name</th>
-                <th className="fs-5">Address</th>
-                <th className="fs-5" >Contact Information</th>
-                <th className="fs-5">Contact Rules</th>
-              </tr>
+            <tr className="">
+              <th className="fs-5">No</th>
+              <th className="fs-5">Legal Name</th>
+              <th className="fs-5">Address</th>
+              <th className="fs-5">Contact Information</th>
+              <th className="fs-5">Contact Rules</th>
+            </tr>
           </thead>
           <tbody>
-            {circleMembers?.map((member, index) => (
-              <tr key={index}>
-                <td>{index+1}</td>
-                <td>{member?.user?.users?.legalName}
-                {member.is_delegate ? <span className="alert alert-primary p-0 px-2 mx-1">Del</span> : null}</td>
-                <td>{member?.user?.users?.address}</td>
-                {/**
-                 * I am not sure how are we going to collect phone numbers of the members?
-                 * consult with Don!
-                 */}
-                <td> +1 32123223 <br /> {member.user?.email} </td>
-                <td>
-                  <div className="container">
-                    <div className="row">
-                      <textarea rows={2} placeholder="Please specify how/when members can reach you out. ">
-                      </textarea>
-                    </div>
-                  </div>
-                </td>
-              </tr>
+            {contactList?.map((member, index) => (
+              <ContactInfoItem
+                key={index}
+                isDelegate={() => isDelegate()}
+                index={index}
+                member={member}
+              />
             ))}
           </tbody>
         </table>
       </div>
     </div>
-  )
+  );
 }
 
 export default MemberContactPage;
---
+++ src/components/contact_info_item.jsx
@@ -0,0 +1,162 @@
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { baseURL } from "../store/conf.js";
+import axios from "axios";
+
+export default function ContactInfoItem({ index, member, isDelegate }) {
+  const [editContactRules, setEditContactRules] = useState(
+    member?.contact_rules
+  );
+  const [editEmail, setEditEmail] = useState(member?.email);
+  const [editPhone, setEditPhone] = useState(member?.phone);
+
+  const AuthUser = useSelector((state) => state.AuthUser.user);
+  const [editing, setEditing] = useState(false);
+  const [editingRules, setEditingRules] = useState(false);
+
+  const handleUpdate = () => {
+    const data = {
+      email: editEmail,
+      phone: editPhone,
+      user: AuthUser.username,
+      contact_rules: editContactRules,
+    };
+
+    let header = { Authorization: `Bearer ${AuthUser.token.access}` };
+    let url = `${window.location.protocol}//${baseURL}/api/contact-info/${member?.id}/`;
+
+    axios
+      .patch(url, data, { headers: header })
+      .then((res) => {
+        setEditPhone(res.data.phone);
+        setEditEmail(res.data.email);
+        setEditContactRules(res.data.contact_rules);
+        setEditing(false);
+        setEditingRules(false);
+      })
+      .catch((err) => console.log("error: ", err));
+  };
+
+  const ContactEditMode = () => {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-12 p-2">
+            <input
+              type="email"
+              defaultValue={editEmail}
+              onChange={(e) => setEditEmail(e.target.value)}
+              placeholder="Email"
+              className="form-control"
+            />
+          </div>
+          <div className="col-12 p-2">
+            <input
+              type="tel"
+              defaultValue={editPhone}
+              onChange={(e) => setEditPhone(e.target.value)}
+              placeholder="Phone"
+              className="form-control"
+            />
+          </div>
+        </div>
+        <div className="row p-2 gap-3 mx-auto">
+          <button
+            onClick={() => handleUpdate()}
+            className="btn btn-primary btn-sm col-5">
+            Update Contact
+          </button>
+          <button
+            onClick={() => setEditing(false)}
+            className="btn btn-secondary btn-sm col-5">
+            cancel
+          </button>
+        </div>
+      </div>
+    );
+  };
+  const ContactRulesEditMode = () => {
+    return (
+      <div className="container">
+        <div className="row">
+          <textarea
+            onChange={(e) => setEditContactRules(e.target.value)}
+            rows={2}
+            defaultValue={editContactRules}
+            placeholder="Please specify how/when members can reach you out."></textarea>
+        </div>
+
+        <div className="row py-1 gap-1">
+          <button
+            onClick={() => handleUpdate()}
+            className="btn  btn-primary btn-sm col-5">
+            Update Rules
+          </button>
+          <button
+            onClick={() => setEditingRules(false)}
+            className="btn btn-secondary btn-sm col-5">
+            cancel
+          </button>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <tr>
+      <td>{index + 1}</td>
+      <td>
+        {member?.member.user?.users?.legalName}
+        {member.member.is_delegate ? (
+          <span className="alert alert-primary p-0 px-2 mx-1">Del</span>
+        ) : null}
+      </td>
+      <td>
+        {member?.address} <br />
+      </td>
+      <td>
+        <div className="container">
+          {editing === true ? (
+            ContactEditMode()
+          ) : (
+            <div className="p-0">
+              <p className="m-0">Email: {editEmail}</p>
+              <p className="m-0">Phone: {editPhone}</p>
+              {isDelegate() === true && (
+                <span
+                  className="  text-primary text-decoration-underline"
+                  style={{ cursor: "pointer" }}
+                  onClick={() => setEditing(true)}>
+                  Edit this section
+                </span>
+              )}
+            </div>
+          )}
+        </div>
+      </td>
+      <td>
+        <div className="container ">
+          {editingRules === true ? (
+            ContactRulesEditMode()
+          ) : (
+            <div className="container p-0 m-0">
+              <div className="row  m-0 mb-1">
+                <textarea
+                  disabled={true}
+                  defaultValue={editContactRules}></textarea>
+              </div>
+              {isDelegate() === true && (
+                <span
+                  className=" text-sm text-primary text-decoration-underline"
+                  style={{ cursor: "pointer" }}
+                  onClick={() => setEditingRules(true)}>
+                  Edit Contact Rules
+                </span>
+              )}
+            </div>
+          )}
+        </div>
+      </td>
+    </tr>
+  );
+}
