b75223239506e62fc9dedf5b230fd693db6e8d93
Zaman Ehsani <zamanehsani@gmail.com>
2025-02-21 22:27:17 +0400

    fixed the claim your seat confirmation message border

--- src/components/signUpConform.jsx
+++ src/components/signUpConform.jsx
@@ -1,21 +1,30 @@
-import check from '../check.png';
-import '../index.css';
+import check from "../check.png";
+import "../index.css";
 
-function SignUpConfirm(){
-    return (
-        <div className="container my-auto">
-            <h3 className="text-success text-center">Almost there... </h3>
-            <div className="col-sm-12">
-                <div className="alert alert-success" >
-                    <div className="image_check">
-                        <img src={check} alt="check"/> 
-                    </div>
-                        <p className="my-2 text-center"> We've sent an email to the address you provided. <br />
-                        Open it and click on the link to confirm and Enter the Floor. </p> 
-                </div>
+function SignUpConfirm() {
+  return (
+    <div className="container my-auto">
+      <h3 className="text-success text-center">Almost there... </h3>
+      <div className="row justify-content-center">
+        <div className="col-sm-12 col-md-4"></div>
+        <div className="col-sm-12 col-md-4">
+          <div className="alert alert-success">
+            <div className="image_check">
+              <img src={check} alt="check" />
             </div>
+            <p className="my-2 text-center">
+              We've sent an email to the address you provided. <br />
+              Open it and click on the link to confirm and Enter the Floor.{" "}
+            </p>
+          </div>
         </div>
-    )
+        <div className="col-sm-12 col-md-4 "></div>
+      </div>
+      <div className="col-sm-12">
+        <div className="container"></div>
+      </div>
+    </div>
+  );
 }
 
 export default SignUpConfirm;
